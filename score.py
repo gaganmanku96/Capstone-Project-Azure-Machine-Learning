365d1824f8d0eb78cb0a93fde99ebdfea50f50ea
gaganmanku96 <gaganmanku96@gmail.com>
2021-02-11 15:29:44 +0530

    updated score script

--- score.py
+++ score.py
@@ -1,7 +1,8 @@
-import json
-import numpy as np
 import os
+import json
 import pickle
+
+import pandas as pd
 
 
 def init():
@@ -12,9 +13,11 @@ def init():
 
 def run(data):
     try:
-        data = np.array(json.loads(data))
+        data = json.loads(data)
+        data = data["data"]
+        data = pd.DataFrame(data)
         result = model.predict(data)
         # You can return any data type, as long as it is JSON serializable.
-        return result.tolist()
+        return json.dumps({"result": result.tolist()})
     except Exception as e:
-        error = str(e)
+        return json.dumps({"error": str(e)})
